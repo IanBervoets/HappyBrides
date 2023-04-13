83513a9ead7589636cbd37ae12cc5b46c4dace8d
Ian Bervoets <ian.bervoets@live.nl>
2023-04-13 18:12:19 +0200

    Updated list page to have an edit mode and made createItem function in JS to make database implementation easier

--- HappyBridesUpdated/Pages/list.cshtml.cs
+++ HappyBridesUpdated/Pages/list.cshtml.cs
@@ -1,11 +1,19 @@
+using System.Security.Principal;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace HappyBridesUpdated.Pages;
 
 public class list : PageModel
 {
+    public bool editing { get; set; }
+    
     public void OnGet()
     {
         
+    }
+
+    public void OnPostEditing()
+    {
+        editing = true;
     }
 }
--- HappyBridesUpdated/wwwroot/js/list.js
+++ HappyBridesUpdated/wwwroot/js/list.js
@@ -17,6 +17,13 @@ $(document).ready(function(){
         $after = $item.next();
         $item.insertAfter($after);
     }
+    
+    function createItem(){
+        var value = $('input[name="itemInput"]').val();
+        $(this).parent().remove();
+        $(".list-group").append('<li class="list-group-item">' + value + '<button class="btn icon delete" type="button"><i class="fas fa-times-circle"></i></button><button class="btn icon down" type="button"><i class="fas fa-arrow-alt-circle-down"></i></button><button type="button" class="btn icon up"><i class="fas fa-arrow-alt-circle-up"></i></button></li>');
+
+    }
 
     $(document).on('click', '.up', function(){
         moveUp($(this).parent());
@@ -27,10 +34,7 @@ $(document).ready(function(){
     })
 
     $(document).on('click', '.confirm', function(){
-        var value = $('input[name="itemInput"]').val();
-        $(this).parent().remove();
-        $(".list-group").append('<li class="list-group-item">' + value + '<button class="btn icon delete" type="button"><i class="fas fa-times-circle"></i></button><button class="btn icon down" type="button"><i class="fas fa-arrow-alt-circle-down"></i></button><button type="button" class="btn icon up"><i class="fas fa-arrow-alt-circle-up"></i></button></li>');
-
+        createItem();
     })
 
     $("#codeModal").modal('show');
