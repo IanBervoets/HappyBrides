486ecf21150c52dd806b8b86a7cdd43927b1c52e
Ian Bervoets <ian.bervoets@live.nl>
2025-03-04 18:22:15 +0100

    Can now add gifts with post instead of js

--- HappyBridesUpdated/Pages/list.cshtml.cs
+++ HappyBridesUpdated/Pages/list.cshtml.cs
@@ -1,42 +1,61 @@
 using System.ComponentModel.DataAnnotations;
 using System.Security.Principal;
+using HappyBridesUpdated.Model;
+using HappyBridesUpdated.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace HappyBridesUpdated.Pages;
 
-public class list : PageModel
+public class GiftsList : PageModel
 {
-    public bool editing { get; set; }
+    private GiftRepository _giftRepository = new GiftRepository();
     
     public string Message { get; set; }
     
     [BindProperty, Required]
     public string Name {get; set;}
+
+    public List<Gift> Gifts = new List<Gift>();
+    
+    public bool IsBride { get; set; }
     
     public void OnGet()
     {
-        
-    }
-
-    public void OnPostEditing()
-    {
-        editing = true;
-    }
-    
-    
-    /*public void OnPostAdd()
-    {
-        string test = Name;
-        if (String.IsNullOrEmpty(test))
+        if (HttpContext.Session.GetString("ID") == null)
         {
-            editing = true;
-            Message = "Add a name";
+            Response.Redirect("/Index");
+        }
+
+        IsBride = CheckIsBride();
+        //string test = HttpContext.Session.GetString("IsBride");
+
+        
+        Gifts = _giftRepository.GetGifts();
+    }
+    
+    public void OnPostAdd()
+    {
+        if (ModelState.IsValid)
+        {
+            string test = Name;
+            Gift gift = new Gift();
+            gift.Name = test;
+            Gifts.Add(gift);
+            //_giftRepository.AddGift(gift);
         }
         else
         {
-            editing = true;
-
+            Message = "Add a name";
         }
-    }*/
+    }
+
+    public bool CheckIsBride()
+    {
+        if (HttpContext.Session.GetString("IsBride") == "true")
+        {
+            return true;
+        }
+        return false;
+    }
 }
--- HappyBridesUpdated/Repositories/GiftRepository.cs
+++ HappyBridesUpdated/Repositories/GiftRepository.cs
@@ -7,6 +7,12 @@ public class GiftRepository
 {
     private static ConnectRepository _connectRepository = new ConnectRepository();
 
+    public List<Gift> GetGifts()
+    {
+        using var connection = _connectRepository.Connect();
+        return connection.Query<Gift>("SELECT * FROM Gifts").ToList();
+    }
+    
     public void AddGift(Gift gift)
     {
         using var connection = _connectRepository.Connect();
